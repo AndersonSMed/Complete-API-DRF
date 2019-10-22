2062db75f9ba6e104ca36a4c9507c3257f672110
Anderson Santos <anderson.santos.medeiros@hotmail.com>
2019-10-22 02:26:28 -0300

    Improving our urls

--- API/urls.py
+++ API/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path, include
-from rest_framework import DefaultRouter
+from rest_framework.routers import DefaultRouter
 from API.users import views as userViews
 
 router = DefaultRouter()
--- API/users/views.py
+++ API/users/views.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.models import User
 from rest_framework import viewsets
-from serializer import UserSerializer
+from .serializer import UserSerializer
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
