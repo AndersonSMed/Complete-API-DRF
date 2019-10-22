09c0dfe51926ec621c5ed48656068473b501f161
Anderson Santos <anderson.santos.medeiros@hotmail.com>
2019-10-22 02:52:30 -0300

    Create insertion test

--- API/users/tests.py
+++ API/users/tests.py
@@ -1,3 +1,23 @@
-from django.test import TestCase
+from django.contrib.auth.models import User
+from rest_framework.test import APITestCase
+from rest_framework.reverse import reverse
+from rest_framework import status
+from .serializer import UserSerializer
 
-# Create your tests here.
+class UsersTests(APITestCase):
+
+    def test_create_user(self):
+        
+        data = {
+            "username": "anderson.med",
+            "email": "anderson.santos.medeiros@hotmail.com",
+            "password": "123456"
+        }
+
+        response = self.client.post(reverse('user-list'), data=data)
+        users = User.objects.filter(**data)
+        serializer = UserSerializer(users.last(), many = False)
+
+        self.assertEqual(response.status_code, 201)
+        self.assertEqual(response.data, serializer.data)
+        self.assertEqual(users.count(), 1)
