300972caea456734912ab6b2521235cc9a7b9278
Anderson Santos <anderson.santos.medeiros@hotmail.com>
2019-10-22 03:16:20 -0300

    Made test for partial update

--- API/users/tests.py
+++ API/users/tests.py
@@ -18,7 +18,7 @@ class UsersTests(APITestCase):
         users = User.objects.filter(**data)
         serializer = UserSerializer(users.last(), many = False)
 
-        self.assertEqual(response.status_code, 201)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertEqual(response.data, serializer.data)
         self.assertEqual(users.count(), 1)
 
@@ -27,7 +27,7 @@ class UsersTests(APITestCase):
         response = self.client.get(reverse('user-list'))
         users = UserSerializer(User.objects.all(), many = True)
 
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.data, users.data)
 
     def test_detail_user(self):
@@ -43,4 +43,24 @@ class UsersTests(APITestCase):
         response = self.client.get(reverse('user-detail', args=[user.pk]))
         
         self.assertEqual(response.data, serializer.data)
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_partial_update_user(self):
+
+        data = {
+            "username": "test",
+            "email": "test@test.com",
+            "password": "123456"
+        }
+
+        new_data = {
+            "username": "test123",
+            "email": "test123@test.com"
+        }
+
+        user = User.objects.create(**data)
+        response = self.client.patch(reverse('user-detail', args=[user.pk]), data = new_data)
+
+        self.assertEqual(response.data.get('username'), new_data.get('username'))
+        self.assertEqual(response.data.get('email'), new_data.get('email'))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
